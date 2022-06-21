19afd6ae19cefbf775b9f1ec3beb3eb9f7ba45a9
Krzysztof Gawlik <krzysztof.gaw@outlook.com>
2022-06-21 19:02:40 +0200

    Modified structures

--- main.cpp
+++ main.cpp
@@ -3,31 +3,38 @@
 using namespace std;
 
 struct FileHeader{
-    char signature[2]; // File structure
-    uint32_t size; // Size of file
-    uint16_t reservedA; // Reserved data A
-    uint16_t reservedB; // Reserved data B
-    uint32_t dataOffset; // Location of the first pixel
+    char signature[2];
+    uint32_t file_size;
+    uint16_t reserved_A;
+    uint16_t reserved_B;
+    uint32_t file_offset_to_pixel_array;
 };
 
 struct BitmapInformationHeader{
-    // Size of self
-    uint32_t size; 
-    // Image dimension in pixels
-    uint32_t width;
-    uint32_t height;
-    // Image settings
-    uint16_t colorPlanes;
-    uint16_t colorDepth;
+    uint32_t dib_header_size; 
+    uint32_t image_width;
+    uint32_t image_height;
+    uint16_t planes;
+    uint16_t bits_per_pixel; // Depth
     uint32_t compression;
-    uint32_t rawSize;
-    //Resolution in pixels per metre
-    int32_t horizontalPpm;
-    int32_t verticalPpm;
-    // Other settings
-    uint32_t colorTable;
-    uint32_t importantColors;
-
+    uint32_t imageSize;
+    uint32_t x_pixels_per_meter; // Horizontal
+    uint32_t x_pixels_per_meter; // Vertical
+    uint32_t colors_in_color_table;
+    uint32_t important_color_count;
+    uint32_t red_channel_bitmask;
+    uint32_t green_channel_bitmask;
+    uint32_t blue_channel_bitmask;
+    uint32_t alpha_channel_bitmask;
+    uint32_t color_space_type;
+    uint32_t color_space_endpoints;
+    uint32_t gamma_for_red_channel;
+    uint32_t gamma_for_green_channel;
+    uint32_t gamma_for_blue_channel;
+    uint32_t intent;
+    uint32_t icc_profile_data;
+    uint32_t icc_profile_size;
+    uint32_t reserved;
 };
 
 struct Pixel{
@@ -49,16 +56,15 @@ int main(void){
     cout << "-------------------------------------" << endl;
     // INFO FROM FILE HEADER
     cout << "SIGNATURE: " << fh.signature << endl;
-    cout << "SIZE: " << fh.size << endl;
-    cout << "RESERVED A: " << fh.reservedA << " & RESERVED B: " << fh.reservedB << endl;
-    cout << "OFFSET: " << fh.dataOffset << endl;
+    cout << "SIZE: " << fh.file_size << endl;
+    cout << "RESERVED A: " << fh.reserved_A << " & RESERVED B: " << fh.reserved_B << endl;
+    cout << "OFFSET: " << fh.file_offset_to_pixel_array << endl;
     cout << "-------------------------------------" << endl;
 
     //INFO FROM INFORMATION HEADER
-    cout << "BIH size: " << bih.size << endl;
-    cout << "Image width: " << bih.width << endl;
-    cout << "Image height: " << bih.height << endl;
-    cout << "Planes: " << bih.colorPlanes << endl;
+    cout << "BIH size: " << bih.dib_header_size << endl <<
+            "Image width: " << bih.image_width << endl <<
+            "Image height: " << bih.image_height << endl;
 
     cout << "-------------------------------------" << endl;
 }
